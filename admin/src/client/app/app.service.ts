4121800f50aedfcf491ac60d517c46be8d2f9ab1
Nikolay <develnk@gmail.com>
2017-01-09 22:23:00 +0600

    Updated Front side

--- admin/src/client/app/app.service.ts
+++ admin/src/client/app/app.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Rx';
-import { $WebSocket, WebSocketSendMode } from 'angular2-websocket/angular2-websocket';
+import { $WebSocket, WebSocketSendMode, WebSocketConfig } from 'angular2-websocket/angular2-websocket';
 
 @Injectable()
 export class AppService {
   private static wsMessages:Subject<Message[]> = new Subject<Message[]>();
   private static _instance:AppService = new AppService();
   private ws:$WebSocket;
-  private wss:WebSocket;
+  private wsConfig:WebSocketConfig;
 
   public static getInstance():AppService {
     return AppService._instance;
@@ -21,32 +21,40 @@ export class AppService {
     if(AppService._instance) {
       throw new Error('Error: Instantiation failed: Use AppService.getInstance() instead of new.');
     }
-    this.ws = new $WebSocket('wss://localhost:' + location.port + '/api/v1');
-    this.wss = new WebSocket("wss://localhost:8081/api/v1");
+    this.wsConfig = {initialTimeout: 500, maxTimeout: 300000, reconnectIfNotNormalClose: true};
+    this.ws = new $WebSocket('wss://localhost:' + location.port + '/api/v1', null, this.wsConfig, 'arraybuffer');
     // this.subscribe();
     AppService._instance = this;
+    this.ws.onMessage(
+        (message: MessageEvent)=> {
+          console.log("onMessage ", message.data);
+        },
+        {autoApply: false}
+    );
+
   }
 
   public send(data:any) {
     console.log('Send through AppService');
-    // if (this.ws.getDataStream().isStopped) {
-    //   this.subscribe();
-    // }
-    this.wss.send(data);
+    if (this.ws.getDataStream().isStopped) {
+      this.subscribe();
+    }
+    this.ws.send(data, WebSocketSendMode.Direct);
+
   }
 
   private subscribe() {
     this.ws.getDataStream().subscribe(
-      (message) => {
-        console.log('Data: ' + message.data);
-        AppService.wsMessages.next([<Message>JSON.parse(message.data)]);
-      },
-      (err) => {console.log('Error: ' + err.message);},
-      () => {
-        console.log('Completed');
-        // this.ws.getDataStream().unsubscribe();
-        // this.wsMessages.unsubscribe();
-      }
+        (msg)=> {
+          console.log("next", msg.data);
+          this.ws.close(false);
+        },
+        (msg)=> {
+          console.log("error", msg);
+        },
+        ()=> {
+          console.log("complete");
+        }
     );
   }
 
